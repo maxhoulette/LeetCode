eddaa7023e36b4a9a3d8237c369f54823fbe07b2
houlette <houlette@student.ubc.ca>
2025-10-28 12:51:09 +0100

    feat: "find target BST" easy problem

---
+++ easy/src/find_target_BST.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <climits>
+#include <unordered_set>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    std::unordered_set<int> set;
+
+    bool findTarget(TreeNode *root, int k)
+    {
+        if (!root)
+            return false;
+        if (set.count(k - root->val))
+            return true;
+        set.insert(root->val);
+
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+};

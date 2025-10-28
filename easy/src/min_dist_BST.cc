94d2ae40bb6d75597832fbf1ce20840324abbb73
houlette <houlette@student.ubc.ca>
2025-10-28 10:18:09 +0100

    feat: "min diff BST" easy problem

---
+++ easy/src/min_dist_BST.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int *previous;
+    int res;
+
+    void dfs(TreeNode *root)
+    {
+        if (root)
+        {
+            dfs(root->left);
+            if (previous)
+                res = min(res, abs(root->val - *previous));
+            previous = &root->val;
+            dfs(root->right);
+        }
+    }
+
+    int minDiffInBST(TreeNode *root)
+    {
+        previous = nullptr;
+        res = INT32_MAX;
+
+        dfs(root);
+        return res;
+    }
+};
